ddc80c6e5b48971706958905092d34d84dc6e60e
marcuzy <t5633@ya.ru>
2019-03-12 23:24:24 +0700

    Add prototypes of classes to control order

--- src/originOrderedImportsRule.ts
+++ src/originOrderedImportsRule.ts
@@ -2,7 +2,7 @@ import * as ts from 'typescript';
 import * as Lint from 'tslint';
 import * as tsutils from 'tsutils';
 
-import { values } from './utils';
+import { values, enumHas } from './utils';
 
 enum BlankLinesOption {
     anyNumber = 'any-number-of-blank-lines',
@@ -12,9 +12,64 @@ enum BlankLinesOption {
 }
 
 enum ModuleType {
-    nodeModules = 'node-modules',
-    customModules = 'custom-modules',
-    //custom = 'custom'
+    Lib = 'lib',
+    User = 'user',
+    CustomRule = 'custom-rule'
+}
+
+class ModulesOrderItem {
+    readonly type: ModuleType;
+    private readonly customRule?: RegExp;
+
+    constructor(optionsItem: string) {
+        if (enumHas(ModuleType, optionsItem)) {
+            this.type = optionsItem as any;
+        } else {
+            this.type = ModuleType.CustomRule;
+            this.customRule = new RegExp(optionsItem);
+        }
+    }
+
+    check(path: string): boolean {
+        if (this.type === ModuleType.CustomRule) {
+            return this.customRule.test(path);
+        }
+
+        const isUserModule = path.startsWith('/') || path.startsWith('.');
+            
+        if (this.type === ModuleType.User && isUserModule) {
+            return true;
+        }
+
+        if (this.type === ModuleType.Lib && !isUserModule) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+class ModulesOrder {
+    private orderPosition: number = 0;
+    private orderItems: Array<ModulesOrderItem>;
+
+    constructor(optionsItems: Array<string>) {
+        this.orderItems = optionsItems.map(_ => new ModulesOrderItem(_));
+    }
+
+    check(path: string): boolean {
+        const index = this.orderItems
+            .slice(this.orderPosition)
+            .findIndex(item => item.check(path));
+
+        if (index === -1) {
+            return false;
+        }
+
+        this.orderPosition += index;
+
+        return true;
+    }
 }
 
 export class Rule extends Lint.Rules.AbstractRule {
@@ -56,9 +111,9 @@ export class Rule extends Lint.Rules.AbstractRule {
                 true, 
                 BlankLinesOption.one,
                 [
-                    ModuleType.nodeModules,
+                    ModuleType.Lib,
                     '^@.+',
-                    ModuleType.customModules
+                    ModuleType.User
                 ]
             ] 
         ],
@@ -68,7 +123,10 @@ export class Rule extends Lint.Rules.AbstractRule {
     };
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-        return this.applyWithWalker(new OriginOrderedImportWalker(sourceFile, this.ruleName, { blankLines: this.blankLines, order: this.order  }));
+        return this.applyWithWalker(new OriginOrderedImportWalker(sourceFile, this.ruleName, { 
+            blankLines: this.blankLines, 
+            modulesOrder: this.modulesOrder
+        }));
     }
     
     private get blankLines(): BlankLinesOption {
@@ -79,56 +137,60 @@ export class Rule extends Lint.Rules.AbstractRule {
         return BlankLinesOption.anyNumber;
     }
 
-    private get order(): Array<any> { // TODO: change type
+    private get modulesOrder(): ModulesOrder { // TODO: change type
         if (this.ruleArguments[1] !== undefined) {
-            return this.ruleArguments[1];
+            
         }
-        
-        return [
-            ModuleType.nodeModules,
-            ModuleType.customModules
-        ];
+
+        const optionsItems = this.ruleArguments[1] !== undefined
+            ? this.ruleArguments[1]
+            : [
+                ModuleType.Lib,
+                ModuleType.User
+            ];
+
+        return new ModulesOrder(optionsItems);
     }
 }
 
 type AnyImportDeclaration = ts.ImportDeclaration | ts.ImportEqualsDeclaration;
 
-enum SourceType {
-    USER,
-    LIB
-}
+// enum SourceType {
+//     USER,
+//     LIB
+// }
 
-const flowRules = {
-    [SourceType.LIB]: [SourceType.USER, SourceType.LIB],
-    [SourceType.USER]: [SourceType.USER]
-};
+// const flowRules = {
+//     [SourceType.LIB]: [SourceType.USER, SourceType.LIB],
+//     [SourceType.USER]: [SourceType.USER]
+// };
 
 const anyImportSyntaxKind = new Set([ts.SyntaxKind.ImportDeclaration, ts.SyntaxKind.ImportEqualsDeclaration]);
 
-class OriginOrderedImportWalker extends Lint.AbstractWalker<{ blankLines: BlankLinesOption, order: Array<ModuleType | string> }> {
-    protected nextSourceTypeMayBe: Array<SourceType> = flowRules[SourceType.LIB];
+class OriginOrderedImportWalker extends Lint.AbstractWalker<{ blankLines: BlankLinesOption, modulesOrder: ModulesOrder}> {
+    //protected nextSourceTypeMayBe: Array<SourceType> = flowRules[SourceType.LIB];
     
-    getFlowRules() {
-        const mt2st = (mt: ModuleType | string): any => {
-            switch (mt) {
-                case ModuleType.nodeModules: return SourceType.LIB;
-                case ModuleType.customModules: return SourceType.USER;
-                default:
-                    return mt;
-            }
-        }
+    // getFlowRules() {
+    //     const mt2st = (mt: ModuleType | string): any => {
+    //         switch (mt) {
+    //             case ModuleType.Lib: return SourceType.LIB;
+    //             case ModuleType.User: return SourceType.USER;
+    //             default:
+    //                 return mt;
+    //         }
+    //     }
 
-        const rules = {};
+    //     const rules = {};
 
-        this.options.order.forEach((mt: ModuleType | string, index) => {
-            rules[mt2st(mt as ModuleType)] = this.options.order
-                    .slice(index)
-                    .map(mt2st);
-        });
+    //     this.options.order.forEach((mt: ModuleType | string, index) => {
+    //         rules[mt2st(mt as ModuleType)] = this.options.order
+    //                 .slice(index)
+    //                 .map(mt2st);
+    //     });
 
-        return rules;
+    //     return rules;
 
-    }
+    // }
 
     public walk(sourceFile: ts.SourceFile) {
         const cb = (node: ts.Node): void => {
@@ -176,9 +238,15 @@ class OriginOrderedImportWalker extends Lint.AbstractWalker<{ blankLines: BlankL
     }
 
     protected check(node: AnyImportDeclaration, source: string): void {
-        const sourceType: SourceType | string = this.getSourceType(source);
+        this.options.modulesOrder.check(source)
+
+        if (!this.options.modulesOrder.check(source)) {
+            this.addFailureAtNode(node, 'Import of node_modules must be higher than custom import.');
+        }
+
+        // const sourceType: SourceType | string = this.getSourceType(source);
     
-        this.checkOrder(node, sourceType);
+        // this.checkOrder(node, sourceType);
 
         // const patternOrderItemIndex = this.options.order
         //     .filter(item => values(ModuleType).indexOf(item) === -1)
@@ -188,84 +256,84 @@ class OriginOrderedImportWalker extends Lint.AbstractWalker<{ blankLines: BlankL
         //         return regexp.test(source);
         //     });
         
-        if (this.options.blankLines !== BlankLinesOption.anyNumber) {
-            this.checkEmptyLine(node, sourceType);
-        }
+        // if (this.options.blankLines !== BlankLinesOption.anyNumber) {
+        //     this.checkEmptyLine(node, sourceType);
+        // }
     }
     
-    protected checkOrder(node: AnyImportDeclaration, sourceType: SourceType | string): void {
-        const valid = (() => {
-            this.nextSourceTypeMayBe.some(item => {
-               if (values(SourceType).indexOf(item) > -1) {
-                   SourceType
-               } 
-            })
-        })();
-        if (!valid) {
-            this.addFailureAtNode(node, 'Import of node_modules must be higher than custom import.');
-        } else {
-            this.nextSourceTypeMayBe = this.getFlowRules()[sourceType];
-        }
-    }
+    // protected checkOrder(node: AnyImportDeclaration, sourceType: SourceType | string): void {
+    //     const valid = (() => {
+    //         this.nextSourceTypeMayBe.some(item => {
+    //            if (values(SourceType).indexOf(item) > -1) {
+    //                SourceType
+    //            } 
+    //         })
+    //     })();
+    //     if (!valid) {
+    //         this.addFailureAtNode(node, 'Import of node_modules must be higher than custom import.');
+    //     } else {
+    //         this.nextSourceTypeMayBe = this.getFlowRules()[sourceType];
+    //     }
+    // }
     
-    protected checkEmptyLine(node: AnyImportDeclaration, sourceType: SourceType): void {
-        if (sourceType === SourceType.USER) {
-            return;
-        }
+    // protected checkEmptyLine(node: AnyImportDeclaration, sourceType: SourceType): void {
+    //     if (sourceType === SourceType.USER) {
+    //         return;
+    //     }
     
-        const nodeLine = ts
-            .getLineAndCharacterOfPosition(
-                this.getSourceFile(),
-                node.getEnd()
-            )
-            .line;
+    //     const nodeLine = ts
+    //         .getLineAndCharacterOfPosition(
+    //             this.getSourceFile(),
+    //             node.getEnd()
+    //         )
+    //         .line;
         
-        const nextNode = tsutils.getNextStatement(node);
+    //     const nextNode = tsutils.getNextStatement(node);
     
-        if (!nextNode || !anyImportSyntaxKind.has(nextNode.kind)) {
-            return;
-        }
+    //     if (!nextNode || !anyImportSyntaxKind.has(nextNode.kind)) {
+    //         return;
+    //     }
     
-        const nextSourceType = this.getSourceType(this.getModuleName(<AnyImportDeclaration>nextNode));
+    //     const nextSourceType = this.getSourceType(this.getModuleName(<AnyImportDeclaration>nextNode));
     
-        if (nextSourceType === SourceType.LIB) {
-            return;
-        }
+    //     if (nextSourceType === SourceType.LIB) {
+    //         return;
+    //     }
     
-        const nextNodeLine = ts
-            .getLineAndCharacterOfPosition(
-                this.getSourceFile(),
-                nextNode.getStart(this.getSourceFile())
-            )
-            .line;
+    //     const nextNodeLine = ts
+    //         .getLineAndCharacterOfPosition(
+    //             this.getSourceFile(),
+    //             nextNode.getStart(this.getSourceFile())
+    //         )
+    //         .line;
     
-        const totalLinesCountBetweenNodes = nextNodeLine - nodeLine - 1;
+    //     const totalLinesCountBetweenNodes = nextNodeLine - nodeLine - 1;
     
-        const blankLinesCount = totalLinesCountBetweenNodes - this.getNodeLeadingCommentedLinesCount(<AnyImportDeclaration>nextNode);
+    //     const blankLinesCount = totalLinesCountBetweenNodes - this.getNodeLeadingCommentedLinesCount(<AnyImportDeclaration>nextNode);
         
-        let failed = false;
-        let whyFailed = '';
+    //     let failed = false;
+    //     let whyFailed = '';
         
-        switch (this.options.blankLines) {
-            case BlankLinesOption.one:
-                failed = blankLinesCount !== 1;
-                whyFailed = 'One blank line required between node_modules import and custom import';
+    //     switch (this.options.blankLines) {
+    //         case BlankLinesOption.one:
+    //             failed = blankLinesCount !== 1;
+    //             whyFailed = 'One blank line required between node_modules import and custom import';
                 
-                break;
-            case BlankLinesOption.no:
-                failed = blankLinesCount !== 0;
-                whyFailed = 'Blank lines between node_modules import and custom import';
+    //             break;
+    //         case BlankLinesOption.no:
+    //             failed = blankLinesCount !== 0;
+    //             whyFailed = 'Blank lines between node_modules import and custom import';
                 
-                break;
-            case BlankLinesOption.atLeastOne:
-                failed = blankLinesCount === 0;
-                whyFailed = 'At least one blank line required between node_modules import and custom import';
-        }
+    //             break;
+    //         case BlankLinesOption.atLeastOne:
+    //             failed = blankLinesCount === 0;
+    //             whyFailed = 'At least one blank line required between node_modules import and custom import';
+    //     }
     
-        if (failed) {
-            this.addFailureAtNode(node, whyFailed);
-        }
-    }
+    //     if (failed) {
+    //         this.addFailureAtNode(node, whyFailed);
+    //     }
+    // }
     
     protected getNodeLeadingCommentedLinesCount(node: AnyImportDeclaration): number {
         const comments = ts.getLeadingCommentRanges(this.getSourceFile().text, node.pos);
@@ -281,23 +349,23 @@ class OriginOrderedImportWalker extends Lint.AbstractWalker<{ blankLines: BlankL
             }, 0);
     }
 
-    protected getSourceType(source: string): SourceType | string {
-        const patternOrderItem = this.options.order
-            .filter(item => values(ModuleType).indexOf(item) === -1)
-            .find(item => {
-                const regexp = new RegExp(item);
+    // protected getSourceType(source: string): SourceType | string {
+    //     const patternOrderItem = this.options.order
+    //         .filter(item => values(ModuleType).indexOf(item) === -1)
+    //         .find(item => {
+    //             const regexp = new RegExp(item);
 
-                return regexp.test(source);
-            });
+    //             return regexp.test(source);
+    //         });
 
-        if (!!patternOrderItem) {
-            return patternOrderItem;
-        }
+    //     if (!!patternOrderItem) {
+    //         return patternOrderItem;
+    //     }
 
-        return source.trim().charAt(0) === '.'
-            ? SourceType.USER
-            : SourceType.LIB;
-    }
+    //     return source.trim().charAt(0) === '.'
+    //         ? SourceType.USER
+    //         : SourceType.LIB;
+    // }
 
     protected removeQuotes(value: string): string {
         if (value && value.length > 1 && (value[0] === `'` || value[0] === `"`)) {
